3fdb12129506073e8a5261b8a8f7581e183d0780
jeff.j.stringer@gmail.com <jeff.j.stringer@gmail.com>
2014-11-14 16:19:42 -0800

    add functions to object to mark answer and calculate score

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 //put your logic here
 var Quiz = {
   initialize: function() {
-    this.score = 0;
-    this.allQuestions = [ {question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1},
-                          {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3},
-                          {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2},
-                          {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0},
-                          {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3}];
+    this.allQuestions = [ {question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1, myAnswer: ''},
+                          {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3, myAnswer: ''},
+                          {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2, myAnswer: ''},
+                          {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0, myAnswer: ''},
+                          {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3, myAnswer: ''}];
+    this.markAnswer = function(question, answer) {
+      var index = this.allQuestions.indexOf(question);
+      this.allQuestions[index].myAnswer = answer;
+    },
     this.nextQuestion = function(question) {
       return this.allQuestions[($.inArray(question, this.allQuestions) + 1) % this.allQuestions.length];
     },
     this.previousQuestion = function(question) {
       return this.allQuestions[($.inArray(question, this.allQuestions) - 1) % this.allQuestions.length];
+    },
+    this.totalScore = function() {
+      var score = 0;
+      var points = 100/(this.allQuestions.length);
+      this.allQuestions.forEach(function(question) {
+        if (question.correctAnswer === question.myAnswer) {
+          score += points;
+        }
+      });
+      return score;
     }
   }
 }
@@ -33,15 +46,20 @@ $(document).ready(function(){
   });
 
   $("button#next").click(function(event) {
+    var select = $("#select");
+    if (select) {
+      select.remove();
+    }
     var isClicked = $("input").data('clicked');
     var clicked = 0;
     currentQuestion.choices.forEach(function(choice) {
       if (isClicked === 'yes') {
         clicked++;
+        return;
       }
     });
     if (clicked === 0) {
-      alert("Please make a selection!");
+      $("div.question").prepend("<p id='select'>Please make a selection.</p>");
       return;
     } 
     currentQuestion = quiz.nextQuestion(currentQuestion);
@@ -59,16 +77,14 @@ $(document).ready(function(){
       $("button#previous").show(); 
     } 
     $("div.question").empty();
-    $("div.question").append(currentQuestion['question']);
+    $("div.question").append("<p>" + currentQuestion['question'] + "</p>");
     currentQuestion.choices.forEach(function(choice) {
       $("div.question").append("<p><input type='radio' data-clicked='no' value=" + currentQuestion.choices.indexOf(choice) + " class='choice' name='choice'>" + " " + choice + " " + "</input></p>");
     });
     $("input[type='radio']").on('click', function(){
       $("input").data('clicked', 'yes');
       var chosen = parseInt($(".choice:checked").val());
-      if (chosen === currentQuestion.correctAnswer) {
-        quiz.score += 20;
-      } 
+      quiz.markAnswer(currentQuestion,chosen); 
     });
   }
 
@@ -76,7 +92,7 @@ $(document).ready(function(){
     $("button#next").remove();
     $("button#previous").remove(); 
     $("div.question").empty();
-    $("div.question").append("<h4>Your final score is: " + quiz.score + "</h4>");
+    $("div.question").append("<h4>Your final score is: " + quiz.totalScore() + "</h4>");
     $("button#again").show();
     $("button#again").click(function() {
       location.reload();
--- specs/specs.js
+++ specs/specs.js
@@ -3,8 +3,25 @@ describe("Quiz", function() {
     it("creates a new quiz object", function() {
       var newQuiz = Object.create(Quiz);
       newQuiz.initialize();
-      newQuiz.score.should.equal(0);
       newQuiz.allQuestions.length.should.equal(5);
+    });
+  });
+
+  describe("markAnswer", function() {
+    it("allow user to mark their answer", function() {
+      var newQuiz = Object.create(Quiz);
+      newQuiz.initialize();
+      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1, myAnswer: ''},
+                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3, myAnswer: ''},
+                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2, myAnswer: ''},
+                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0, myAnswer: ''},
+                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3, myAnswer: ''}
+
+      ];
+      var currentQuestion = newQuiz.allQuestions[0];
+      var chosen = 2;
+      newQuiz.markAnswer(currentQuestion, chosen);
+      currentQuestion.myAnswer.should.equal(2);
     });
   });
   
@@ -12,11 +29,11 @@ describe("Quiz", function() {
     it("selects the next item in the allQuestions array", function() {
       var newQuiz = Object.create(Quiz);
       newQuiz.initialize();
-      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1},
-                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3},
-                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2},
-                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0},
-                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3}
+      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1, myAnswer: ''},
+                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3, myAnswer: ''},
+                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2, myAnswer: ''},
+                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0, myAnswer: ''},
+                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3, myAnswer: ''}
 
       ];
       var currentQuestion = newQuiz.allQuestions[0];
@@ -29,16 +46,31 @@ describe("Quiz", function() {
     it("selects the previous item in the allQuestions array", function() {
       var newQuiz = Object.create(Quiz);
       newQuiz.initialize();
-      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1},
-                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3},
-                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2},
-                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0},
-                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3}
+      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1, myAnswer: ''},
+                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3, myAnswer: ''},
+                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2, myAnswer: ''},
+                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0, myAnswer: ''},
+                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3, myAnswer: ''}
 
       ];
       var currentQuestion = newQuiz.allQuestions[2];
       var previous = newQuiz.previousQuestion(currentQuestion);
       previous.should.equal(newQuiz.allQuestions[1]);
+    });
+  });
+
+    describe("totalScore", function() {
+    it("totals the score at the end of the quiz", function() {
+      var newQuiz = Object.create(Quiz);
+      newQuiz.initialize();
+      newQuiz.allQuestions = [{question: "What is 1 + 1?", choices: ["3", "2", "1", "0"], correctAnswer:1, myAnswer:1},
+                              {question: "What color is the sky?", choices: ["Red", "Orange", "Green", "Blue"], correctAnswer:3, myAnswer:3},
+                              {question: "What are the colors of the Canadian Flag?", choices: ["Red and Green", "Yellow, Red and Black", "Red and White", "Red, White and Blue"], correctAnswer:2, myAnswer:3},
+                              {question: "Who is the president of the United States of America?", choices: ["Obama", "Reagan", "Bush", "Warshington"], correctAnswer:0, myAnswer:0},
+                              {question: "What state do you live in?", choices: ["Warshington", "Alabama", "Hawaii", "Oregon"], correctAnswer:3, myAnswer:3}
+
+      ];
+      newQuiz.totalScore().should.equal(80);
     });
   });
 });
